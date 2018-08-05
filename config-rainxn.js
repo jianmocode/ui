49e3943795cf2fcf3e26e35c2218fbfb7d040045
trheyi <zhuce@diancloud.com>
2018-08-05 11:50:33 +0800

    + rainxn

---
+++ config-rainxn.js
@@ -0,0 +1,17 @@
+config = {
+	mina:{
+		target: false,
+		priority:0,
+		server:"http://www.rainxn.com",
+		domain:"www.rainxn.com",
+		project:"default",
+		appid:'153296193941950',
+		secret:'0f76cf48202e2709174f89f561ddec24'
+	},
+	wxapp:{
+		"cli":"/Applications/wechatwebdevtools.app/Contents/Resources/app.nw/bin/cli",
+		"appid": "wx8f62a28a0ddd0946",
+		"secret": "c29abedee3ba815d4322bbca8ba80322"
+	}
+};
+module.exports = config
