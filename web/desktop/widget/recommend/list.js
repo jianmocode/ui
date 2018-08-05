e82493bd14a6092460b18dcaff9bdf8ad74210e9
trheyi <zhuce@diancloud.com>
2018-08-05 11:46:09 +0800

    + 翻页

--- web/desktop/widget/recommend/list.js
+++ web/desktop/widget/recommend/list.js
@@ -3,12 +3,26 @@ let com = Page({
 	data:{},
 	onReady: function( params ) {
 		
+		// 翻页
 		$('.pagination .more').click(()=>{
 
 			let vars = params.var || {};
 			let slug = vars.slug || 0;
-			let url = '/widget/recommend/list/' + slug;
-			console.log('pagination more click', url,  params, this.data );
+			let page = $('.pagination .more').data('next') || 1; 
+			if ( page === false ) {
+				return ;
+			}
+
+			let url = '/widget/recommend/list/' + slug + '?page=' + page;
+
+			$('.pagination .more').html('加载中...');
+			$.get(url, function(html) {
+				html = '<div>' + html + '</div>';
+				var items = $(html).find('.articles').html();
+				$('.articles').append( items );
+				$('.pagination .more').html('加载更多');
+			});
+
 		});
 	}
 })
