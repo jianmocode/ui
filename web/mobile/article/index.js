0c584f08c0e374efa61126f8488f57fe347c865e
trheyi <zhuce@diancloud.com>
2018-08-09 15:29:14 +0800

    fix width

--- web/mobile/article/index.js
+++ web/mobile/article/index.js
@@ -19,6 +19,8 @@ Page({
 	 * @return {[type]} [description]
 	 */
 	initRecommendsNavbar: function() {
+
+
 		$('.jm-recommends-navbar a').click(function(){
 			$('.jm-recommends-navbar a').removeClass('nav-active');
 			$(this).addClass('nav-active');
@@ -27,8 +29,22 @@ Page({
 				$('.jm-recommends-body').html('<div uk-spinner class="uk-padding-small"></div>');
 				$('.jm-recommends-body').load( link );
 			}
-			// return true;
-			return false;
+			return true;
+			// return false;
 		})
+
+
+		// Fix width
+		let width = $('.jm-recommends-navbar').width();
+		let w = 0;
+		$('.jm-recommends-navbar a').each((idx, elm)=>{
+			w += $(elm).outerWidth();
+		})
+		
+		let offset = width - w;
+
+		if ( offset > 0 ){
+			$('.jm-recommends-navbar').append('<a style="width:'+ offset +'px"></a>');
+		}
 	}
 })
