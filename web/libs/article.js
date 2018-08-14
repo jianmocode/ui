201b1419acff40ab9f2298c0564603f30735e666
trheyi <zhuce@diancloud.com>
2018-08-14 22:39:54 +0800

    优化视频呈现

--- web/libs/article.js
+++ web/libs/article.js
@@ -6,25 +6,39 @@ class Article {
 	
 	constructor( elm ) {
 		this.article = $(elm);
+		this.maxw = this.article.children('.jm-article-content').innerWidth();
 	}
 
 	init() {
 
 		// fix cvideo 
-		this.article.find('.mp-cvideo').each( function(idx, elm ){
+		this.article.find('.mp-cvideo').each( (idx, elm ) => {
 			
-			let w = $(elm).outerWidth();
-			let h = $(elm).outerHeight();
-			let aw = parseInt($(elm).attr('width') || w) || 1;
-			let ah = parseInt($(elm).attr('height') || h) 
-			let ratio = ah/aw;
+			let w = this.maxw;
+			// let h = $(elm).innerHeight();
+			// let aw = parseInt($(elm).attr('width') || w) || 1;
+			// let ah = parseInt($(elm).attr('height') || h) 
+			// // let ratio = ah/aw;
+			let ratio = 9/16;
 			let fixh = w * ratio;
 
 			$(elm).height(fixh);
+			$(elm).width(w);
+
+			// fix iframe
 			$(elm).children('iframe').height(fixh);
 			$(elm).children('iframe').width(w);
 			$(elm).children('iframe').attr('height', fixh);
 			$(elm).children('iframe').attr('width', w);
+
+
+			// fix video
+			$(elm).children('video').height(fixh);
+			$(elm).children('video').width(w);
+			$(elm).children('video').attr('height', fixh);
+			$(elm).children('video').attr('width', w);
+
+			// console.log( w, fixh );
 		})
 	}
 }
