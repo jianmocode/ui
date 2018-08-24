69103c69b98db42a60cb28c6a2b6f007a8afe011
trheyi <zhuce@diancloud.com>
2018-08-25 01:14:02 +0800

    + 站点地图

---
+++ config-xueyuan.js
@@ -0,0 +1,17 @@
+config = {
+	mina:{
+		target: false,
+		priority:0,
+		server:"http://xueyuan.jianmoapp.com/",
+		domain:"xueyuan.jianmoapp.com",
+		project:"default",
+		appid:'152825288992645',
+		secret:'f7dfebc7c1d2c8ac8086767bf5c4b7aa'
+	},
+	wxapp:{
+		"cli":"/Applications/wechatwebdevtools.app/Contents/Resources/app.nw/bin/cli",
+		"appid": "wx8f62a28a2d0946",
+		"secret": "c29abed33815d4322bbca8ba80322"
+	}
+};
+module.exports = config
