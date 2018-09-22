2c4fce447dc940debc06a0229b97f0ebabc5023d
trheyi <zhuce@diancloud.com>
2018-09-22 18:32:23 +0800

    + tabs 切换

---
+++ web/desktop/home/forms/profile.js
@@ -0,0 +1,51 @@
+import {Utils} from '../../../libs/utils.js';
+import {Validate} from '../../../libs/validate.js';
+
+let $utils = new Utils();
+let web = getWeb();
+let $$ = UIkit.util;
+
+Page({
+	data:{},
+	smscodeTimer:0,
+	validator: null,
+
+	onReady: function( get ) {
+
+		var that = this;
+
+		// 错误提醒框关闭事件
+		try {
+			$$.on('.uk-alert', 'hide', ()=>{
+				setTimeout(()=>{$utils.parentHeight();}, 500);
+			});
+		} catch( e ) { console.log( 'Error @Alert Hide Event', e); }
+
+		// 表单验证类
+		try {
+			this.validator = new Validate({
+				form:'.iframe-form',
+				change: ( error, element)=>{ $utils.parentHeight(); },
+				error: (message, extra)=>{ $utils.parentHeight(); },
+				complete: ()=>{ $utils.parentHeight(); }
+			});
+
+		} catch( e ) { console.log( 'Error @Validate', e); }
+
+
+		// 图形验证码
+		$('.image.vcode').click(function(){
+			that.changeVcode();
+		});
+	},
+
+
+	/**
+	 * 更换图片验证码
+	 * @return {[type]} [description]
+	 */
+	changeVcode: function(){
+		var api = '/_api/xpmsns/user/user/vcode?width=150&height=40&size=20&t=' + Date.parse(new Date()); 
+		$('.image.vcode img').attr('src', api);
+	},
+})
--- web/desktop/home/index.js
+++ web/desktop/home/index.js
@@ -2,24 +2,33 @@
 let web = getWeb();
 Page({
 	data:{},
-	curr_ad:0,
-	maxW:0,
 	onReady: function( param ) {
-		this.maxW = $('.article').innerWidth();
-		this.fixImages();
-	},
 
+		// Tab 切换
+		$('.jm-tabs li>a').click(function(event) {
+			
+			// 设定为Loading提示
+			$('#forms .loading').removeClass('uk-hidden');
+			$('#forms .loaded').addClass('uk-hidden');
 
-	/**
-	 * 修复图片被压缩问题
-	 * @return {[type]} [description]
-	 */
-	fixImages: function() {
-		$('.mp-cimage').each(( idx, em)=>{
-			if ( $(em).attr('width') > this.maxW ) {
-				$(em).attr('height', '');
-				$(em).css('height', '');
-			}
+			let link = $(this).attr('href');
+			let html =  '<iframe width="100%" height="100%" src="'+ link +'" frameborder="0"  scrolling="no"></iframe>';
+			$('#forms .iframe').html(html);
+			$('#forms .loaded').removeClass('uk-hidden'); // 打开Page
+
+			$('#forms iframe').load(function( event ){
+				let frameH = $(this).contents().find("body").height();
+				$(this).attr('height', frameH);
+				$(this).css('height', frameH);
+				$('#forms .loading').addClass('uk-hidden');
+			});
 		});
+
+		let uri = window.location.href.split('#');
+		let form = uri[1] || 'profile';
+		let link = '/home/forms/'+form;
+		$('.jm-tabs li').removeClass('uk-active');
+		$('.jm-tabs a[href=\''+link+'\']').parent().addClass('uk-active');
+		$('.jm-tabs a[href=\''+link+'\']').trigger('click');
 	}
 })
