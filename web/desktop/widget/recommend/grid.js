e4bc1747fa96c7be082178f7c7e0a40a443e1514
Max <zhuce@diancloud.com>
2018-11-02 04:49:37 +0800

    + Grid 组件

---
+++ web/desktop/widget/recommend/grid.js
@@ -0,0 +1,40 @@
+let web = getWeb();
+let com = Page({
+	data:{},
+	onReady: function( params ) {
+
+		console.log( params );
+		
+		// 翻页
+		$('.pagination .more').click(()=>{
+
+			let vars = params.var || {};
+			let slug = vars.slug || 0;
+			let page = $('.pagination .more').attr('data-next') || 1; 
+			if ( page == false ) {
+				return ;
+			}
+
+			let url = '/widget/recommend/grid/' + slug + '?page=' + page;
+
+			$('.pagination .more').html('加载中...');
+			$.get(url, function(html) {
+				html = '<div>' + html + '</div>';
+				var items = $(html).find('.album').html();
+				$('.album').append( items );
+
+				// 翻页
+				var next = $(html).find('.pagination .more').attr('data-next');
+				if ( next ) {
+					$('.pagination .more').attr('data-next', next).html('加载更多');
+				} else {
+					$('.pagination .more').remove();
+					$('.pagination').append('<hr  class="uk-divider-icon">');
+				}
+			});
+
+		});
+	}
+})
+
+module.exports = com;
