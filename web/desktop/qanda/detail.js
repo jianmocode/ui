f94d4d8066aa6ef3e8feb21fb6b2836047f816dc
xiewendao123 <xiewendao123@foxmail.com>
2019-01-28 20:50:55 +0800

    ok

--- web/desktop/qanda/detail.js
+++ web/desktop/qanda/detail.js
@@ -2,10 +2,15 @@ let web = getWeb();
 
 Page({
       data: {},
+      is_answer_input_show: false,
       onReady: function () {
             const _that = this;
 
             _that.handleHoverUserAvatar();
+            _that.handleClickQuestionShowAll();
+            _that.handleClickWriteAnswer();
+            _that.handleClickSubmitAnswer();
+            _that.handleClickAnswerAgree();
       },
       handleHoverUserAvatar: function () {
             const _that = this;
@@ -39,5 +44,177 @@ Page({
                               )
                   }
             )
+      },
+      handleClickQuestionShowAll: function () {
+            const _that = this;
+
+            $('.btn_show_all_question').on('click', function () {
+                  $(this)
+                        .parent()
+                        .find('.question_summary')
+                        .toggle();
+
+                  $(this)
+                        .parent()
+                        .find('.question_detail')
+                        .toggle();
+            })
+      },
+      handleClickWriteAnswer: function () {
+            const _that = this;
+
+            $('.btn_write').on('click', function () {
+                  if (_that.is_answer_input_show) {
+                        $('.answer_input').slideUp();
+                        $(this)
+                              .find('.text')
+                              .text('写回答');
+                        _that.is_answer_input_show = false;
+                  } else {
+                        $('.answer_input').slideDown();
+                        $(this)
+                              .find('.text')
+                              .text('不写了');
+                        _that.is_answer_input_show = true;
+                  }
+            })
+      },
+      submitAnswerForm: function () {
+            $.ajax({
+                  type: "post",
+                  url: "/_api/xpmsns/qanda/answer/create",
+                  dataType: "json",
+                  data: $('#answer_form').serialize(),
+                  success: function (response) {
+                        if (response._id) {
+                              UIkit.notification({
+                                    message: '回答成功',
+                                    status: 'success',
+                                    pos: 'bottom-right'
+                              });
+
+                              setTimeout(() => {
+                                    window.location.reload();
+                              }, 1000);
+                        } else {
+                              UIkit.notification({
+                                    message: response.message,
+                                    status: 'danger',
+                                    pos: 'bottom-right'
+                              })
+                        }
+                  },
+                  error: function (err) {
+                        console.log(err);
+                  }
+            });
+      },
+      handleClickSubmitAnswer: function () {
+            const _that = this;
+
+            let isValidated = false;
+
+            $('.btn_submit').on('click', function () {
+
+                  if ($('.answer_content').val()) {
+                        isValidated = true;
+                  } else {
+                        UIkit.notification({
+                              message: '请输入答案内容',
+                              status: 'warning',
+                              pos: 'bottom-right'
+                        })
+                  }
+
+                  if (isValidated) {
+                        _that.submitAnswerForm();
+                  }
+            })
+      },
+      createAgree: function (data) {
+            $.ajax({
+                  type: "post",
+                  url: "/_api/xpmsns/comment/agree/create",
+                  dataType: "json",
+                  data: data,
+                  success: function (response) {
+                        console.log(response);
+
+                        // if (response._id) {
+                        //       UIkit.notification({
+                        //             message: '回答成功',
+                        //             status: 'success',
+                        //             pos: 'bottom-right'
+                        //       });
+
+                        //       setTimeout(() => {
+                        //             window.location.reload();
+                        //       }, 1000);
+                        // } else {
+                        //       UIkit.notification({
+                        //             message: response.message,
+                        //             status: 'danger',
+                        //             pos: 'bottom-right'
+                        //       })
+                        // }
+                  },
+                  error: function (err) {
+                        console.log(err);
+                  }
+            });
+      },
+      removeAgree: function () {
+            $.ajax({
+                  type: "post",
+                  url: "/_api/xpmsns/comment/agree/remove",
+                  dataType: "json",
+                  data: $('#answer_form').serialize(),
+                  success: function (response) {
+                        if (response._id) {
+                              UIkit.notification({
+                                    message: '回答成功',
+                                    status: 'success',
+                                    pos: 'bottom-right'
+                              });
+
+                              setTimeout(() => {
+                                    window.location.reload();
+                              }, 1000);
+                        } else {
+                              UIkit.notification({
+                                    message: response.message,
+                                    status: 'danger',
+                                    pos: 'bottom-right'
+                              })
+                        }
+                  },
+                  error: function (err) {
+                        console.log(err);
+                  }
+            });
+      },
+      handleClickAnswerAgree: function () {
+            const _that = this;
+
+            $('.btn_agree').on('click', function () {
+                  $(this).hide();
+                  $(this)
+                        .parent()
+                        .find('.btn_agree_clicked')
+                        .show();
+
+                  _that.createAgree({
+                        outer_id: $(this).data('id'),
+                        origin: 'answer'
+                  });
+            })
+
+            $('.btn_agree_clicked').on('click', function () {
+                  $(this).hide();
+                  $(this)
+                        .parent()
+                        .find('.btn_agree')
+                        .show();
+            })
       }
 })
--- web/desktop/qanda/index.js
+++ web/desktop/qanda/index.js
@@ -7,6 +7,7 @@ Page({
 
             _that.handleClickAsk();
             _that.handleClickHideAsk();
+            _that.handleClickPublish();
       },
       showAskModal: function () {
             $('.ask_wrap')
@@ -34,6 +35,58 @@ Page({
 
             $('.btn_delete_modal').on('click', function () {
                   _that.hideAskModal();
+            })
+      },
+      submitAskForm: function () {
+            $.ajax({
+                  type: "post",
+                  url: "/_api/xpmsns/qanda/question/create",
+                  dataType: "json",
+                  data: $('#ask_form').serialize(),
+                  success: function (response) {
+                        if (response._id) {
+                              UIkit.notification({
+                                    message: '提问成功',
+                                    status: 'success',
+                                    pos: 'bottom-right'
+                              });
+
+                              setTimeout(() => {
+                                    window.location.href = `/qanda/detail/${response.question_id}`;
+                              }, 1000);
+                        } else {
+                              UIkit.notification({
+                                    message: response.message,
+                                    status: 'danger',
+                                    pos: 'bottom-right'
+                              })
+                        }
+                  },
+                  error: function (err) {
+                        console.log(err);
+                  }
+            });
+      },
+      handleClickPublish: function () {
+            const _that = this;
+
+            let isValidated = false;
+
+            $('.btn_publish').on('click', function () {
+
+                  if ($('.question_title').val()) {
+                        isValidated = true;
+                  } else {
+                        UIkit.notification({
+                              message: '请输入问题标题',
+                              status: 'warning',
+                              pos: 'bottom-right'
+                        })
+                  }
+
+                  if (isValidated) {
+                        _that.submitAskForm();
+                  }
             })
       }
 })
