26af43c39187347eeeae88f3ee20634b77c3a42e
Max <zhuce@diancloud.com>
2019-02-06 21:15:22 +0800

    fix login bug

--- web/libs/user.js
+++ web/libs/user.js
@@ -12,13 +12,26 @@ class User {
 
 		// 更新用户资料
 		let fields = {
-			nickname: ( $elm, value )=>{ $elm.find('[user-nickname]').html( value ); },
+			nickname: ( $elm, value, user )=>{ 
+                
+                if ( value == null ) {
+                    var moblie = user["mobile"] ? user["mobile"] : '13000000000';  
+                    value=moblie.replace(/(\d{3})\d{4}(\d{4})/, '$1****$2');
+                }
+
+                $elm.find('[user-nickname]').html( value ); 
+            },
 			headimgurl: ( $elm, value ) => {
+                if ( value == null ) return;
+
 				if ( typeof value == 'object' && value.url ) {
 					$elm.find('[user-headimgurl]').attr( 'src', value.url ); 
 				}
 			},
 			bgimgurl: ( $elm, value ) => {
+                
+                if ( value == null ) return;
+
 				if ( typeof value == 'object' && value.url ) {
 					$elm.find('[user-bgimgurl]').attr( 'src', value.url ); 
 				}
@@ -38,7 +51,7 @@ class User {
 			for ( var field in fields ) {
 				let fn = fields[field];
 				let val = user[field];
-				try { fn( $elm, val); } catch(e){
+				try { fn( $elm, val, user); } catch(e){
 					console.log( 'update field error', e );
 				}
 			}
