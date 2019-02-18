e21aca683e2e556826d8d00ecd5f34d7a0c0326c
Max <zhuce@diancloud.com>
2019-02-18 23:06:39 +0800

    + 文章给审核列表

--- web/desktop/admin/pages/article/audit.js
+++ web/desktop/admin/pages/article/audit.js
@@ -1,5 +1,34 @@
 Page({
 	data:{},
 	onReady: function( get ) {
-	},
+    },
+
+    /**
+     * 读取文章详情
+     * @param {event} event 
+     */
+    openArticle: function(event) {
+        let $elm = $(event.target)
+        let id = $elm.attr("data-id");
+        $('#articleDetail').modal({
+            keyboard: false
+        });
+    },
+
+    /**
+     * 通过发布请求
+     * @param {event} event 
+     */
+    resolve: function( event ) {
+        console.log( event );
+    },
+
+    /**
+     * 驳回发布请求
+     * @param {Event} event 
+     */
+    reject: function( event ) {
+        console.log( event );
+    }
+
 })
