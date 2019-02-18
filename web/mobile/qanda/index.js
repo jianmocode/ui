3b6db7de9af97419db0226af9457e23219e26922
xiewendao123 <xiewendao123@foxmail.com>
2019-02-18 20:21:45 +0800

    ok

--- web/mobile/qanda/index.js
+++ web/mobile/qanda/index.js
@@ -1,9 +1,26 @@
+import {
+      $$
+} from '../../libs/component'
+
+$$.import(
+      'editor/image',
+      'editor/html'
+)
+
 let web = getWeb();
 
 Page({
       data: {},
+      loadEditor: function () {
+            try {
+                  $$('editor[type=html]').HtmlEditor({})
+            } catch (e) {
+                  console.log('Error @HtmlEditor', e)
+            }
+      },
       onReady: function () {
             const _that = this
 
+            _that.loadEditor()
       },
 })
