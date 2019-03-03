79b39356450bed7fe656555da101f8e50b8ce9ca
Max <zhuce@diancloud.com>
2019-03-03 17:10:07 +0800

    更新数据

--- web/desktop/event/detail.js
+++ web/desktop/event/detail.js
@@ -15,20 +15,48 @@ $$.import(
 let web = getWeb()
 
 Page({
-      data: {},
-      current_page_ranked: 1,
-      current_page_recommend: 1,
-      has_load_all: false,
-      current_type: 'ranked',
-      loadEditor: function () {
-            try {
-                  // HtmlEditor
-                  $$('editor[type=html]').HtmlEditor({});
-            } catch (e) {
-                  console.log('Error @HtmlEditor', e);
+    data: {},
+    onReady: function ( data ) {
+
+        let content = this.data.event.content || '';
+        this.loadContent( content );
+      
+    },
+    loadContent: function() {
+        
+        let $dom = $('.event-content .content').children();
+        if ( $dom.length  > 1 ) {
+            // 计算目录
+            let catalog = [];
+            $dom.each( (idx, elm  )=>{
+                if ( $(elm).prop("tagName")  == 'H1' ) {
+                    let name = $(elm).text();
+                    let id = `catalog-${idx}`;
+                    $(elm).attr('id', id);
+                    catalog.push({
+                        id:id,
+                        name:name,
+                        index:idx
+                    });
+                }
+            });
+
+            // 呈现目录
+            if ( catalog.length > 0  ){
+                for ( let i in catalog ) {
+                    let c = catalog[i];
+                    let n = catalog[i+1];
+
+                    $('.event-content .catalog .items').append(
+                        `<li><a href="#${c.id}"  uk-scroll="duration:200">${c.name}</a></li>`
+                    );
+                }
+                $('.event-content .catalog').removeClass('uk-hidden');
             }
-      },
-      onReady: function () {
-            const _that = this;
-      }
+        }
+
+        $('.event-content .content').removeClass('uk-hidden');
+        console.log( $dom.length );
+
+    }
 })
