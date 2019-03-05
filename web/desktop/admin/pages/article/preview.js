51f5eae626e2079ed3c980a7d68d7e0f110c2a33
Max <zhuce@diancloud.com>
2019-03-05 11:33:21 +0800

    升级 preview

--- web/desktop/admin/pages/article/preview.js
+++ web/desktop/admin/pages/article/preview.js
@@ -281,5 +281,59 @@ Page({
             UIkit.notification({message: '删除失败', status: 'danger', pos: 'top-right'});
             window.page.unlockAction();
         });
+    },
+
+    copyToClipboard: function(event) {
+
+        var elem = document.getElementById("article-location");
+
+        var targetId = "_hiddenCopyText_";
+        var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+        var origSelectionStart, origSelectionEnd;
+        if (isInput) {
+            // can just use the original source element for the selection and copy
+            target = elem;
+            origSelectionStart = elem.selectionStart;
+            origSelectionEnd = elem.selectionEnd;
+        } else {
+            // must use a temporary form element for the selection and copy
+            target = document.getElementById(targetId);
+            if (!target) {
+                var target = document.createElement("textarea");
+                target.style.position = "absolute";
+                target.style.left = "-9999px";
+                target.style.top = "0";
+                target.id = targetId;
+                document.body.appendChild(target);
+            }
+            target.textContent = elem.textContent;
+        }
+        // select the content
+        var currentFocus = document.activeElement;
+        target.focus();
+        target.setSelectionRange(0, target.value.length);
+        
+        // copy the selection
+        var succeed;
+        try {
+              succeed = document.execCommand("copy");
+        } catch(e) {
+            succeed = false;
+        }
+        // restore original focus
+        if (currentFocus && typeof currentFocus.focus === "function") {
+            currentFocus.focus();
+        }
+        
+        if (isInput) {
+            // restore prior selection
+            elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+        } else {
+            // clear temporary content
+            target.textContent = "";
+        }
+
+        UIkit.notification({message: `复制成功`, status: 'success', pos: 'top-right'});
+        return succeed;
     }
 })
